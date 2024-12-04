f04e5e3309edf7c63de6930e2bb81ae7092f6a13
atauber27 <atauber27@gmail.com>
2024-12-04 15:40:22 -0500

    pie charts

--- analyze_preds.py
+++ analyze_preds.py
@@ -59,6 +59,42 @@ def get_error_categories():
     plt.ylabel('True labels')
     plt.show()
 
-get_error_categories()
+# THIS SHOULD ONLY BE CALLED BY pie_charts()!!!!
+def plot_pie_charts(batch_df, batch_num):
+    genres = ["rb", "pop", "misc", "rock", "country"]
+    fig, axes = plt.subplots(2, 3)
+    axes = axes.flatten()
+
+    for idx, (row_index, row) in enumerate(batch_df.iterrows()):
+        values = row[genres].values
+        ax = axes[idx]
+        ax.pie(
+            values,
+            labels=genres,
+            autopct='%1.1f%%',
+            startangle=140
+        )
+        ax.set_title(row['target'])
+    
+    # Hide unused subplots if batch size < 6
+    for ax in axes[len(batch_df):]:
+        ax.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+def pie_charts():
+    fname = 'preds_distilgpt_all_labels.tsv'
+    df = pd.read_csv(fname, sep='\t')
+    batch_size = 6
+    num_batches = (len(df) + batch_size - 1) // batch_size
+    for batch_num in range(num_batches):
+        start_idx = batch_num * batch_size
+        end_idx = min((batch_num + 1) * batch_size, len(df))
+        batch_df = df.iloc[start_idx:end_idx]
+        plot_pie_charts(batch_df, batch_num + 1)
+
+
+pie_charts()
         
         
